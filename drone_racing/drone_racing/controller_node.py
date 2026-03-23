c105d00cee56fac131fa6443599ba80aacb9d6bc
William <william.heikkila@abo.fi>
2026-03-23 16:49:43 +0200

    updated test

--- drone_racing/drone_racing/controller_node.py
+++ drone_racing/drone_racing/controller_node.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.timer import Timer
 from std_msgs.msg import String
 import time
-from tello_ros.tello_msgs.srv import TelloAction
+from tello_msgs.srv import TelloAction
 
 class DroneController(Node):
 
@@ -10,17 +11,22 @@ class DroneController(Node):
         super().__init__('drone_controller')
         self.cli = self.create_client(TelloAction, 'tello_action')
         self.req = TelloAction.Request()
-        time.sleep(2)
-        self.req.cmd = 'takeoff'
-        self.cli.call(self.req)
-        time.sleep(5)
-        self.req.cmd = 'land'
-        self.cli.call(self.req)
-    
-    def send_request(self, a, b):
-        self.req.a = a
-        self.req.b = b
-        return self.cli.call_async(self.req)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.timer_time: int = 0
+
+
+    def timer_callback(self):
+        self.timer_time += 1
+
+        if self.timer_time == 2:
+            self.req.cmd = 'takeoff'
+            self.cli.call_async(self.req)
+            print("takeoff")
+        
+        if self.timer_time == 7:
+            self.req.cmd = 'land'
+            self.cli.call_async(self.req)
+            print("land")
 
 
 
--- drone_racing/setup.py
+++ drone_racing/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'drone_test = drone_racing.controller_node:main',
         ],
     },
 )
