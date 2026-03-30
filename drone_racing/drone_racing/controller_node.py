89cba396c00d13d7d540f87ba9b25c3eaef164bb
William <william.heikkila@abo.fi>
2026-03-30 14:16:43 +0300

    drone controller improvements

--- drone_racing/drone_racing/controller_node.py
+++ drone_racing/drone_racing/controller_node.py
@@ -1,57 +1,92 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.timer import Timer
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 import time
 from tello_msgs.srv import TelloAction
+from tello_msgs.msg import TelloResponse
 
 class DroneController(Node):
 
     def __init__(self):
         super().__init__('drone_controller')
         self.cli = self.create_client(TelloAction, 'tello_action')
+        self.vel_publisher = self.create_publisher(Twist, "cmd_vel", 1)
+        self.response_subscriber = self.create_subscription(TelloResponse, "tello_response", self.response_callback, 1)
+
+        self.takeoff_complete = False
+        self.landing = False
+        self.i = 0
 
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for tello_action service...')
+        
+        #Crank my hog
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+        self.vel_publisher.publish(msg)
 
-        self.timer = self.create_timer(1.0, self.timer_callback)
-        self.timer_time: int = 0
-        self.landed = False
 
-    def timer_callback(self):
-        self.timer_time += 1
+        self.takeoff()
+        
+        #Start tick timer
+        self.tick_timer = self.create_timer(0.1, self.tick)
+    
+    def tick(self):
+        if self.takeoff_complete:
+            move_time = 20
+            if self.i < move_time:
+                self.move_forward()
+            elif not self.landing:
+                self.landing = True
+                self.land()
+            
+            self.i += 1
 
-        # Step 1: enter SDK mode
-        if self.timer_time == 1:
-            req = TelloAction.Request()
-            req.cmd = 'command'
-            future = self.cli.call_async(req)
-            future.add_done_callback(self.callback_response)
-            self.get_logger().info("command mode")
 
-        # Step 2: takeoff
-        if self.timer_time == 5:
-            req = TelloAction.Request()
-            req.cmd = 'takeoff'
-            future = self.cli.call_async(req)
-            future.add_done_callback(self.callback_response)
-            self.get_logger().info("takeoff")
+    def response_callback(self, msg: TelloResponse):
+        if msg.rc == 1:
+            self.takeoff_complete = True
+
+    def takeoff_timer_callback(self):
+        if not self.takeoff_complete:
+            self.takeoff_complete = True
+            self.get_logger().info("Takeoff Complete")
+
+            self.i = 0
             self.landed = False
 
-        if self.timer_time == 15:
-            req = TelloAction.Request()
-            req.cmd = 'cw 360'
-            future = self.cli.call_async(req)
-            future.add_done_callback(self.callback_response)
-            self.get_logger().info("turn")
+    def move_forward(self):
+        msg = Twist()
+        msg.linear.x = 1.0
+        self.vel_publisher.publish(msg)
+        self.get_logger().info("Forward")
+    
+    def land(self):
+        self.stop()
+        time.sleep(1.0)
+        self.landed = True
+        req = TelloAction.Request()
+        req.cmd = "land"
+        self.cli.call_async(req)
+        self.get_logger().info("Landing")
 
-        # Step 3: land
-        if self.timer_time > 20 and not self.landed:
-            req = TelloAction.Request()
-            req.cmd = 'land'
-            future = self.cli.call_async(req)
-            future.add_done_callback(self.callback_response)
-            self.get_logger().info("land")
+    def takeoff(self):
+        req = TelloAction.Request()
+        req.cmd = "takeoff"
+        future = self.cli.call_async(req)
+        self.get_logger().info("Takeoff")
+    
+    def stop(self):
+        req = TelloAction.Request()
+        req.cmd = "stop"
+        future = self.cli.call_async(req)
+        self.get_logger().info("Stop")
 
     def callback_response(self, future):
         try:
@@ -64,14 +99,14 @@ class DroneController(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = DroneController()
+    drone_controller = DroneController()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(drone_controller)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    drone_controller.destroy_node()
     rclpy.shutdown()
 
 
